40ca447bc3d4ed6af2fd287ae807a8fddfeef5ed
Robin Nicole <robinnicole@192.168.1.100>
2020-11-14 00:50:04 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"; 
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
 
-function App() {
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array 
+}
+class App extends Component {
+    state = {
+      people: [], 
+      name: '',
+      mail: '', 
+      shuffled_list: []
+  }
+  removePerson(ix)
+  {
+    this.state.people.splice(ix, 1)
+    this.setState({people: this.state.people})
+  }
+  renderVisitors()
+  {
+    return this.state.people.map((e, ix) =>  <li key={ix}> {e.name}, {e.mail}  <button onClick={() => this.removePerson(ix)}> DELETE</button> </li>)
+  }
+  onChange(e)
+  {
+    let new_state = {}
+    new_state[e.target.name] = e.target.value
+    this.setState(new_state)
+  }
+  isDisabled()
+  {
+    return (this.state.name.length > 0) & (this.tate.mail.length > 0)
+  }
+  renferBoureauVictimes()
+  {
+    let to_process = this.state.shuffled_list ;
+    return to_process.map((val, ix) => 
+    <li>
+      {to_process[ix].name} Gives a gift to   {to_process[(ix + 1) % to_process.length].name} 
+    </li>
+    )
+  }
+  shufflePersons()
+  {
+    let people = [...this.state.people]
+    shuffleArray(people)
+    console.log(people)
+    this.setState({shuffled_list: people})
+  }
+
+
+  handleSubmit(e)
+  {
+    let people = {name: this.state.name, mail: this.state.mail}
+    if (emailIsValid(this.state.mail))
+    {
+    this.setState({people: [...this.state.people, people], name: '', mail: ''})
+    }
+    else
+    {
+      alert('invalid mail')
+    }
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>Name: 
+        <input type="text" name="name" onChange={(e) => this.onChange(e)} value={this.state.name}/>
+        </label>
+        <label>Mail: 
+        <input type="text" name="mail" onChange={(e) => this.onChange(e)} value={this.state.mail}/>
+        </label>
+      </form>
+      <button onClick={(e) => this.handleSubmit(e)} disabled={(!this.state.name)||(!this.state.mail) }> Submit </button>
+      <ul>
+      {this.renderVisitors()}
+      </ul>
+      <button onClick={() => this.shufflePersons()}> Shuffle </button>
+      <ul>
+        {this.renferBoureauVictimes()}
+      </ul>
     </div>
   );
+  }
 }
 
 export default App;
